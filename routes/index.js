f834917e3f5f02cca73f9e3cd67c9afdf546dc64
Ian Hung <ianmenghung@gmail.com>
2014-09-23 19:28:47 -0400

    just checking that i can push to github

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index.html', { title: 'Express' });
+  res.render('index.html', { title: 'Ian is testing the world' });
 });
 
 module.exports = router;
